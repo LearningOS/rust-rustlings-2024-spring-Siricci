121ab3b685455d6d57446c60088d9d11a96f8ab1
Yuqi Shi <1193132204@qq.com>
2024-04-09 13:53:08 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,7 +16,9 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity:i32) -> i32 {
+    
+}
 
 // Don't modify this function!
 #[test]
